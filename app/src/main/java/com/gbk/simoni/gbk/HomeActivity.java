9106b74b44d1bddf6b2d02c1d5852ce69aecc97f
dsimon01 <dsimon01@dcs.bbk.ac.uk>
2018-10-27 22:21:35 +0100

    code can update the status of an order

--- app/src/main/java/com/gbk/simoni/gbk/HomeActivity.java
+++ app/src/main/java/com/gbk/simoni/gbk/HomeActivity.java
@@ -7,22 +7,81 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-/* LATER FUNCTION
-    public void accept(View view){
-        // Parse query to edit value of status to confirmed
-        // change the value of the button to Mark as ready
+    String orderSelected;
+
+
+
+    // Parse query to edit value of status to accepted
+    // change the value of the button to Mark as ready - for now done inside the on item click listener
+    public void onAccept(View view){
+        Log.i("TO ACCEPT", orderSelected);
+        ParseQuery<ParseObject> updateOrderStatus = ParseQuery.getQuery("Order");
+        updateOrderStatus.whereEqualTo("OrderID", Integer.parseInt(orderSelected));
+        updateOrderStatus.setLimit(1);
+        updateOrderStatus.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null && objects != null){
+                    for (ParseObject object : objects){
+                        Log.i("STATUS RETRIEVED", object.getString("Status"));
+                        object.put("Status", "accepted");
+                        object.saveInBackground();
+                    }
+                }else {
+                    Log.i("ERRRRRRRRR", "ERROR");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-*/
+
+
+
+
+
+    // Parse query to edit value of status to ready
+    // change the visibility of the button to invisible - for now done inside the on item click listener
+    public void onMarkAsReady(View view){
+        Log.i("TO COMPLETE", orderSelected);
+        ParseQuery<ParseObject> updateOrderStatus = ParseQuery.getQuery("Order");
+        updateOrderStatus.whereEqualTo("OrderID", Integer.parseInt(orderSelected));
+        // updateOrderStatus.setLimit(1);
+        updateOrderStatus.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null && objects != null){
+                    for (ParseObject object : objects){
+                        Log.i("STATUS RETRIEVED", object.getString("Status"));
+                        object.put("Status", "completed");
+                        object.saveInBackground();
+                    }
+                }else {
+                    Log.i("ERRRRRRRRR", "ERROR");
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+
+
+
 
 
     @Override
@@ -30,7 +89,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         final ListView ordersListView = findViewById(R.id.ordersListView);
-        final ArrayList<String> orderDetails = new ArrayList<>();
+        //final ArrayList<String> orderDetails = new ArrayList<>();
         final ArrayList<String> forAdapter = new ArrayList<>();
         final ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, forAdapter);
 
@@ -39,21 +98,61 @@ public class HomeActivity extends AppCompatActivity {
         ordersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-                String orderSelected = forAdapter.get(position);
+                orderSelected = forAdapter.get(position);
                 Log.i("CLICKED", orderSelected);
+
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Order");
+
                 query.whereEqualTo("OrderID", Integer.parseInt(orderSelected));
+
                 query.findInBackground(new FindCallback<ParseObject>() {
                     @Override
                     public void done(List<ParseObject> objects, ParseException e) {
                         if (e == null) {
+
                             Log.i("FINDINBACKGROUND", "Retrieved " + objects.size() + " objects");
+
                             if (objects.size() > 0) {
+
                                 for (ParseObject object : objects) {
-                                    Log.i("FOUND", object.getString("Status"));
-                                    Log.i("FOUND", object.getString("TableNumber"));
-                                    Log.i("FOUND", object.get("OrderID").toString());
-                                    Log.i("FOUND", object.get("Item").toString());
+                                    String status = object.getString("Status");
+                                    String tableNumber = object.getString("TableNumber");
+                                    String OrderID = object.get("OrderID").toString();
+                                    String item = object.get("Item").toString();
+
+                                    TextView orderN = findViewById(R.id.orderNumberTextView);
+                                    orderN.setText(OrderID);
+                                    orderN.setVisibility(view.VISIBLE);
+
+                                    TextView table = findViewById(R.id.tableNumberTextView);
+                                    table.setText(tableNumber);
+                                    table.setVisibility(view.VISIBLE);
+
+                                    TextView items = findViewById(R.id.itemTextView1);
+                                    items.setText(item);
+                                    items.setVisibility(view.VISIBLE);
+
+                                    if (status.matches("new")) {
+
+                                        Button accept = findViewById(R.id.accept);
+                                        accept.setVisibility(view.VISIBLE);
+                                        Button markAsCompleted = findViewById(R.id.markAsReady);
+                                        markAsCompleted.setVisibility(view.GONE);
+
+                                    }else if(status.matches("accepted")){
+
+                                        Button markAsCompleted = findViewById(R.id.markAsReady);
+                                        markAsCompleted.setVisibility(view.VISIBLE);
+                                        Button accept = findViewById(R.id.accept);
+                                        accept.setVisibility(view.GONE);
+                                    }else{
+
+                                        Button markAsCompleted = findViewById(R.id.markAsReady);
+                                        markAsCompleted.setVisibility(view.GONE);
+                                        Button accept = findViewById(R.id.accept);
+                                        accept.setVisibility(view.GONE);
+
+                                    }
                                 }
                             }
                         } else {
@@ -89,8 +188,8 @@ public class HomeActivity extends AppCompatActivity {
 }
 
 
-// TODO: Make "view.VISIBLE" the order information in the home activity - they need to update as the user is pressing on another order ID
-// TODO: MEMORY REFRESHER FOR CURRENT CODE: the code is able to retrieve info on order basis
+// TODO: Updated the last version of code with this one where order updates are now possible :)
+// TODO: MEMORY REFRESHER FOR CURRENT CODE: the code is able to retrieve info and update on order basis
 
 
 
@@ -101,51 +200,5 @@ public class HomeActivity extends AppCompatActivity {
 // On the list just have to add the order number > when the user taps on the list item, display all other info on the side screen as per design
 //https://stackoverflow.com/questions/32138061/how-to-run-a-parse-query-in-background-or-schedule-it-in-android
 //https://www.programcreek.com/java-api-examples/?class=com.parse.ParseQuery&method=findInBackground
+// ec2-35-178-79-238.eu-west-2.compute.amazonaws.com > Server link
 
-/*
-                String orderSelected = forAdapter.get(position);
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Order");
-                query.whereEqualTo("OrderID", orderSelected);
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    @Override
-                    public void done(List<ParseObject> objects, ParseException e) {
-
-                        if (e == null){
-                            if (objects.size() > 0){
-
-                                Log.i("QUERY", objects.toString());
-
-                                for (ParseObject object : objects){
-
-                                    orderDetails.add(object.get("OrderID").toString());
-                                    orderDetails.add(object.getString("TableNumber"));
-                                    orderDetails.add(object.getString("Status"));
-                                    orderDetails.add(object.get("Item").toString());
-
-                                    TextView orderN = findViewById(R.id.orderNumberTextView);
-                                    orderN.setText(orderDetails.get(0));
-                                    orderN.setVisibility(view.VISIBLE);
-
-                                    TextView table = findViewById(R.id.tableNumberTextView);
-                                    table.setText(orderDetails.get(1));
-                                    table.setVisibility(view.VISIBLE);
-
-                                    TextView items = findViewById(R.id.itemTextView1);
-                                    items.setText(orderDetails.get(3));
-                                    items.setVisibility(view.VISIBLE);
-
-                                    Button accept = findViewById(R.id.accept);
-                                    accept.setVisibility(view.VISIBLE);
-
-                                }
-                                Log.i("ORDER DETAILS", orderDetails.toString());
-                            }
-                        }else {
-                            Log.i("ERRRRRRRRR", "ERROR");
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
- */
